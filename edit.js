5f1b5f4a441b36d6a3a682131ab1ff3d0d3d24ab
David <haz.d@outlook.com>
2022-06-05 15:16:12 +0300

    fix multiple editing

--- edit.js
+++ edit.js
@@ -1,8 +1,13 @@
 (() => {
     document.designMode = 'on';
     const indicator = document.createElement('div');
-    indicator.innerHTML = `<span style="position: fixed; left: 50%; transform: translateX(-50%); bottom: 2em; border-radius: 10px; background: #12e78e; font-weight: 500; color: #16161a; transform-origin: 0 50%; white-space: nowrap; z-index: 100; padding: .5em 1em;">Editing Page - Press ESC to quit!</span>`;
-    document.body.appendChild(indicator);
+    indicator.setAttribute('id', 'easyedit');
+
+    if (!document.getElementById('easyedit')) {
+        indicator.innerHTML = `<span style="position: fixed; left: 50%; transform: translateX(-50%); bottom: 2em; border-radius: 10px; background: #12e78e; font-weight: 500; color: #16161a; transform-origin: 0 50%; white-space: nowrap; z-index: 100; padding: .5em 1em;">Editing Page - Press ESC to quit!</span>`;
+        document.body.appendChild(indicator);
+    }
+
     document.addEventListener('keyup', e => {
         if (e.key === 'Escape') {
             document.designMode = 'off';
